bb3ed9361666c90ea05aeb58162d04dcf275f94b
Cale <14838@holbertonstudents.com>
2026-08-18 16:25:42 +1000

    added a +1 and made single node return 0

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,17 +1,17 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - Counts the number of leaves in a tree
+ * binary_tree_nodes - Counts the number of nodes in a tree
  * @tree: Pointer to tree
  *
  * Return: Number of nodes with a single child
  */
-size_t binary_tree_nodes(const binary_tree_t *tree);
+size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
 	if (tree->left == NULL || tree->right == NULL)
-		return (1);
-	return (binary_tree_nodes(tree->right)
+		return (0);
+	return (1 + binary_tree_nodes(tree->right)
 	 + binary_tree_nodes(tree->left));
 }
