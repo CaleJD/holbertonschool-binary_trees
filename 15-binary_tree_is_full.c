a640d7df7f6e7f448f269d35858235aa7f53ad02
Cale <14838@holbertonstudents.com>
2026-08-18 19:09:05 +1000

    moved end return

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -14,7 +14,8 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (1);
 	if (tree->left == NULL || tree->right == NULL)
 		return (0);
+	return (1);
+
 	binary_tree_is_full(tree->left);
 	binary_tree_is_full(tree->right);
-	return (1);
 }
