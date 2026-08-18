cf43fc21934d8261af39b7e26146b03e1e5b2f50
Cale <14838@holbertonstudents.com>
2026-08-18 19:53:05 +1000

    attempt fix

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -27,10 +27,10 @@ static int binary_tree_height_helper(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_is_perfect - Checks if tree is perfect
- * @tree: Pointer to tree
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: pointer to the root node
  *
- *Return: 1 if true, 0 if false
+ * Return: 1 if the tree is perfect, or 0 if it is not
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
@@ -43,10 +43,10 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree->left == NULL || tree->right == NULL)
 		return (0);
 
-	if (binary_tree_height_helper(tree->left) !=
-	binary_tree_height_helper(tree->right))
+	if (binary_tree_height_helper(tree->left)
+		!= binary_tree_height_helper(tree->right))
 		return (0);
 
-	return (binary_tree_is_full(tree->left) &&
-	binary_tree_is_full(tree->right));
+	return (binary_tree_is_perfect(tree->left)
+		&& binary_tree_is_perfect(tree->right));
 }
