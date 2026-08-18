d36fe62ce59ea11db45e2cfa4f4a7507322e3b77
Cale <14838@holbertonstudents.com>
2026-08-18 16:21:03 +1000

    added file 13 and updated header

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - Counts the number of leaves in a tree
+ * @tree: Pointer to tree
+ *
+ * Return: Number of nodes with a single child
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree);
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL || tree->right == NULL)
+		return (1);
+	return (binary_tree_leaves(tree->right)
+	 + binary_tree_leaves(tree->left));
+}
