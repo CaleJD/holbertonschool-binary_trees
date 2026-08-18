4c46c9dd21b86c8cc8b6e8d41b41c97b22d40b50
Cale <14838@holbertonstudents.com>
2026-08-18 16:54:20 +1000

    reversed minus order

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -16,6 +16,6 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 	size1 = binary_tree_balance(tree->left);
 	size2 = binary_tree_balance(tree->right);
-	size_dif = (size1 - size2);
+	size_dif = (size2 - size1);
 	return (size_dif);
 }
