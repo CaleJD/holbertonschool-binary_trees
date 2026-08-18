939906e5da038c4b876ffd6334af3576cd22e08a
Cale <14838@holbertonstudents.com>
2026-08-18 12:12:11 +1000

    added file 3 and updated header

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,8 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+#include <stdlib.h>
+
 /**
  * struct binary_tree_s - binary tree node
  *
@@ -22,5 +24,6 @@ typedef struct binary_tree_s binary_tree_t;
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
