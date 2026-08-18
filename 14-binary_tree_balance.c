cd936ea7d2a2c61b2f950ed3c5269cb12dab39d6
Cale <14838@holbertonstudents.com>
2026-08-18 17:38:30 +1000

    removed an int

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,12 +10,10 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	int size1;
 	int size2;
-	int size_dif;
 
 	if (tree == NULL)
 		return (0);
 	size1 = binary_tree_balance(tree->left);
 	size2 = binary_tree_balance(tree->right);
-	size_dif = (size2 - size1);
-	return (size_dif);
+	return (size1 - size2);
 }
