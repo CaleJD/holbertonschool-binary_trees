5cc62668a71ee1e39d269ec4e87ae2bd83dd25dc
Cale <14838@holbertonstudents.com>
2026-08-18 19:06:58 +1000

    added edge case for only root

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -10,6 +10,8 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
+	if (tree->left == NULL && tree->right == NULL && tree->parent == NULL)
+		return (1);
 	if (tree->left == NULL || tree->right == NULL)
 		return (0);
 	binary_tree_is_full(tree->left);
